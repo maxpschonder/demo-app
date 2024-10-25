71c12f32187431118e4834a54d1641ff1788b287
Max Pschonder <max.pschonder@hivemindtechnologies.com>
2024-10-25 09:21:52 +0200

    add monitoring endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express, { Express, Request, Response } from "express";
+import express_prom_bundle from "express-prom-bundle";
 import os from "os";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const metricsMiddleware = express_prom_bundle({ includeMethod: true });
+
+app.use(metricsMiddleware);
 
 app.get("/", (req: Request, res: Response) => {
   res.send(`Demo App served by: ${os.hostname()}`);
